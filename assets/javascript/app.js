a8aab94b30b1ef33f39074cc87e571af4a458814
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-10-20 17:09:17 -0400

    Added topics array

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var topics = [
+    "Pasta",
+    "Sushi",
+    "Curry",
+    "Dim Sum",
+    "Cake",
+    "Pho",
+    "Ramen",
+    "Fried Chicken",
+    "Pizza",
+    "Seafood",
+    "Fried Rice",
+    "Taco",
+    "Burrito",
+    "Ice Cream",
+    "Barbeque",
+    "Fish and Chips",
+    "Kebab",
+    "Chow Mein",
+    "Ice Cream",
+    "Pie",
+    "Cheese"
+]
